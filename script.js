31236e71cfe4e0c3c2ac6038319732d535b3aad2
Ahmedijaz11 <101427579+Ahmedijaz11@users.noreply.github.com>
2022-05-17 13:31:28 +0500

    Comments Added

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ const overlay = document.querySelector('.overlay');
 const btnclose = document.querySelector('.close-modal');
 const btnopen = document.querySelectorAll('.show-modal');
 
+//Function to close the model box
 
 const closeModal = function(){
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
 }
 
-
+//Function to Open the Model Box
 const openModal = function(){
 
     modal.classList.remove('hidden');
@@ -19,6 +20,7 @@ const openModal = function(){
 }
 
 
+//Loop for every button to open modal
 
 for(let i = 0; i<btnopen.length; i++){
 
@@ -26,9 +28,14 @@ for(let i = 0; i<btnopen.length; i++){
 }
 
 
+//calling function to close modal and overlay
 
 btnclose.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+
+
+//Close modal using specfic key press
 
 document.addEventListener('keydown', function(e){
             
