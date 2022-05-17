a49074863faeefc39e6e3c27a3cd69ccce07bfce
Ahmedijaz11 <101427579+Ahmedijaz11@users.noreply.github.com>
2022-05-17 13:28:20 +0500

    KeyPress Handeling

--- script.js
+++ script.js
@@ -30,3 +30,10 @@ for(let i = 0; i<btnopen.length; i++){
 btnclose.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
+document.addEventListener('keydown', function(e){
+            
+    if(e.key === 'Escape' && !modal.classList.contains('hidden')){
+
+        closeModal();  
+    }
+});
